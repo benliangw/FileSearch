ea691fa5e2ec1b1168479b0999e8d5032f594ffd
saholic <benliangw@163.com>
2018-01-03 22:37:02 +0800

    Start my project

---
+++ Filesearch/src/main/java/NKUSE/Filesearch/App.java
@@ -0,0 +1,13 @@
+package NKUSE.Filesearch;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
